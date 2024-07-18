76b8cea8c5ae91097ffafa7d00dcac11b05d18f3
Matheus <mreinheimer0@gmail.com>
2024-07-17 22:01:31 -0300

    chore: Enable fallback for dynamic product page and add loading state

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import Stripe from 'stripe'
 
 import { stripe } from '@/lib/stripe'
@@ -17,6 +18,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <h1>Carregando...</h1>
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -38,7 +45,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [{ params: { id: 'prod_QTzmzufqDPASBl' } }],
-    fallback: false,
+    fallback: true,
   }
 }
 
